b9d8f46c200f9c1e849e96685ad3ec00ccc87e58
Ashley <afletcher679@gmail.com>
2025-02-19 22:02:00 -0500

    Add firebase authentication dependency. Upgrade gradle plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -64,10 +64,8 @@ kotlin {
             // TODO: Add the dependencies for Firebase products you want to use
             // When using the BoM, don't specify versions in Firebase dependencies
             implementation(libs.firebase.analytics)
+            implementation(libs.firebase.ui.auth)
 
-
-            // Add the dependencies for any other desired Firebase products
-            // https://firebase.google.com/docs/android/setup#available-libraries
         }
         commonMain.dependencies {
             implementation(compose.runtime)
